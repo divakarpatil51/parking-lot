6df6fbc6701ddb2e8250de30de95161ad1ab713a
Divakar Patil <divakar.patil@globant.com>
2020-03-02 18:03:47 +0530

    Added logging utility

---
+++ src/main/java/com/coditas/log/MessageLogger.java
@@ -0,0 +1,13 @@
+package com.coditas.log;
+
+/**
+ * Used to delegate logging to the currently configured default logging.
+ */
+public class MessageLogger implements Logger {
+
+	@Override
+	public void log(String message) {
+		System.out.println(message);
+	}
+
+}
